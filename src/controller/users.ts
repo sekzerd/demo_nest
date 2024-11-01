febd392871c97d58b586bf7570a9e1a378eacfc5
WayneSu <sekzerd@outlook.com>
2024-11-01 16:36:38 +0800

    format code

--- src/controller/users.ts
+++ src/controller/users.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+// import { UsersService } from '@/service/users';
 
 @Controller('users')
 export class UsersController {
   @Get()
   findAll(): string {
-    return 'This action returns all cats';
+    // let user  = new UsersService();
+    // retrun  UsersService.findAll();
+    const p = { name: 'jack', age: 18, status: false };
+    return JSON.stringify(p);
   }
 }
